e2b8b4d2c7aa7ee6ff081d2055738da4f0918b91
furszy <mati_fur@hotmail.com>
2017-08-22 14:19:32 -0300

    minor fix

--- init.sh
+++ init.sh
@@ -4,6 +4,11 @@ echo "Cloning ContactsApp"
 git clone https://github.com/furszy/IoP-connect-contacts.git ContactsApp
 echo "Cloning Connect-Chat"
 git clone https://github.com/furszy/Connect-Chat.git Connect-chat
-echo "Now build"
-gradle build
-echo "Ready!"
+if [[ -z "${ANDROID_HOME}" ]]; then
+  echo "Android home undefined, use the android studio please"
+else
+  echo "Now build"
+  gradle build
+  echo "Ready!"
+fi
+
