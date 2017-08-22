29cda8de77d04b9558b90646742b6ead85cf7de0
furszy <mati_fur@hotmail.com>
2017-08-22 14:13:05 -0300

    build added to init file

--- init.sh
+++ init.sh
@@ -4,4 +4,6 @@ echo "Cloning ContactsApp"
 git clone https://github.com/furszy/IoP-connect-contacts.git ContactsApp
 echo "Cloning Connect-Chat"
 git clone https://github.com/furszy/Connect-Chat.git Connect-chat
+echo "Now build"
+gradle build
 echo "Ready!"
