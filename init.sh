8fa92c61de57566f20cc70ef495696072c98cf53
furszy <mati_fur@hotmail.com>
2017-08-22 14:10:48 -0300

    initial commit

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Preparing Connect enviroment.."
+echo "Cloning ContactsApp"
+git clone https://github.com/furszy/IoP-connect-contacts.git ContactsApp
+echo "Cloning Connect-Chat"
+git clone https://github.com/furszy/Connect-Chat.git Connect-chat
+echo "Ready!"
